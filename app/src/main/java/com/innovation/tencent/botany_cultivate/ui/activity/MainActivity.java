ea21d61c8fe127279988ec12700475dc42ca0ec8
梁嘉腾 <602689817@qq.com>
2015-10-14 16:25:56 +0800

    线程池工具

--- app/src/main/java/com/innovation/tencent/botany_cultivate/ui/activity/MainActivity.java
+++ app/src/main/java/com/innovation/tencent/botany_cultivate/ui/activity/MainActivity.java
@@ -2,10 +2,12 @@ package com.innovation.tencent.botany_cultivate.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.innovation.tencent.botany_cultivate.R;
+import com.innovation.tencent.botany_cultivate.utils.ThreadPoolUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,27 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Task task2 = new Task("task2", 100);
+        Task task1 = new Task("task1", 100);
+        ThreadPoolUtil.getLongPool().execute(task1);
+        ThreadPoolUtil.getLongPool().execute(task2);
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public class Task implements Runnable {
+        private String name;
+        private int time;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public Task(String name, int time) {
+            this.name = name;
+            this.time = time;
         }
-
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void run() {
+            Log.d("MainActivity","test....................");
+            for (int i = 0; i < time; i++) {
+                int t = time - i;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(name + ":" + "剩下" + t + "s");
+            }
+        }
     }
 }
