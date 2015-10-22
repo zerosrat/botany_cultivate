96844cb2ad0b4204ac2713784bcd260094e01850
zero <827016252@qq.com>
2015-10-22 13:29:10 +0800

    Detail Activity 相关

---
+++ app/src/main/java/com/innovation/tencent/botany_cultivate/ui/activity/DetailActivity.java
@@ -0,0 +1,37 @@
+package com.innovation.tencent.botany_cultivate.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.innovation.tencent.botany_cultivate.R;
+import com.innovation.tencent.botany_cultivate.base.BaseActivity;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getRootView() {
+        return R.layout.activity_detail;
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setComposition() {
+
+    }
+}
